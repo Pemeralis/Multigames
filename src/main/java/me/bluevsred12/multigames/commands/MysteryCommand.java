1b8787bad997a613c53acd4d2ad6332e732fab0d
BlueVsRed12 <dgr8stgamer141@gmail.com>
2020-02-10 10:39:47 -0600

    Refactored internal testing system

--- src/main/java/me/bluevsred12/multigames/commands/MysteryCommand.java
+++ src/main/java/me/bluevsred12/multigames/commands/MysteryCommand.java
@@ -8,24 +8,45 @@ import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+
 public class MysteryCommand implements CommandExecutor {
     private Multigames plugin;
 
+    private Map<String, Test> runnableTests;
+
     public MysteryCommand(Multigames plugin) {
         this.plugin = plugin;
+        runnableTests = new HashMap<>();
+
+        initializeTests();
+    }
+
+    private void initializeTests() {
+        runnableTests.put("particles", this::testParticles);
+        runnableTests.put("titles", this::testTitles);
     }
 
     @Override
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
         if (args.length < 1) {
-            sender.sendMessage("Not enough arguments!");
+            sender.sendMessage("Available tests:");
+            sendAvailableTests(sender);
             return true;
         }
         String argument = args[0];
-        if (argument.equalsIgnoreCase("particles")) testParticles(sender);
-        else if (argument.equalsIgnoreCase("titles")) testTitles(sender);
+        Test test = runnableTests.get(argument);
+        if (test != null) test.run(sender);
         else sender.sendMessage(argument + " is not a testable argument!");
         return true;
+    }
+
+    private void sendAvailableTests(CommandSender sender) {
+        for (String testName : runnableTests.keySet()) {
+            sender.sendMessage(testName);
+        }
     }
 
     private void testTitles(CommandSender sender) {
@@ -54,4 +75,8 @@ public class MysteryCommand implements CommandExecutor {
         );
         player.sendMessage("Poof!");
     }
+}
+
+interface Test {
+    void run(CommandSender sender);
 }
