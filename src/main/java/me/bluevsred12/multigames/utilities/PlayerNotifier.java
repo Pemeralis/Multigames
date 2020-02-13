1d6150a2e49ac9709271da0f1e215e4c8811b0eb
BlueVsRed12 <dgr8stgamer141@gmail.com>
2020-02-13 08:41:49 -0600

    Class for system of sending player notifications via titles, subtitles, and action bar texts.

--- src/main/java/me/bluevsred12/multigames/utilities/PlayerNotifier.java
+++ src/main/java/me/bluevsred12/multigames/utilities/PlayerNotifier.java
@@ -1,4 +1,13 @@
 package me.bluevsred12.multigames.utilities;
 
+import org.bukkit.entity.Player;
+
+import java.util.Set;
+
 public class PlayerNotifier {
+    private Set<Player> receivers;
+
+    public PlayerNotifier(Set<Player> players) {
+        receivers = players;
+    }
 }
